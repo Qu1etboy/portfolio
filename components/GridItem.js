7f18105d8e2be041929873b922453185b1f8e6e4
Non Weerawong <weerawong.v&ku.th>
2022-06-15 22:06:50 +0700

    add weather app and todo app to works

--- components/GridItem.js
+++ components/GridItem.js
@@ -1,11 +1,18 @@
-export const WorkGridItem = () => {
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const WorkGridItem = ({ children, thumbnail, title, url }) => {
     return (
-        <div className="cursor-pointer">
-            <div className="h-[200px] w-[200px] bg-red-400 rounded-lg"></div>
-            <div className="text-center p-5">
-                <h1 className="font-bold text-2xl mb-5">Title</h1>
-                <p>Description</p>
+        <Link href={url}>
+            <div className='cursor-pointer'>
+                <div className="h-[200px] w-[200px] bg-slate-50 rounded-lg overflow-hidden">
+                    <Image src={thumbnail} width='200' height='200'/>
+                </div>
+                <div className="text-center p-5 h-[200px] w-[200px]">
+                    <h1 className="font-bold text-2xl mb-5">{title}</h1>
+                    <p>{children}</p>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
--- pages/Works.js
+++ pages/Works.js
@@ -5,6 +5,8 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { FadeIn } from "../components/Animations";
 import { WorkGridItem } from "../components/GridItem";
+import weatherApp from '../public/images/works/weather-app.png';
+import todo from '../public/images/works/todo.png'
 
 const Works = () => {
     const [theme, toggleTheme] = useContext(ThemeContext); 
@@ -20,13 +22,12 @@ const Works = () => {
                     <div className="flex flex-1 flex-col justify-center items-center mt-16 md:mt-32">
                         <h className='text-5xl font-bold mb-20'>My Works</h>
                         <div className="grid grid-cols-1 md:grid-cols-3 gap-24">
-                            <WorkGridItem />
-                            <WorkGridItem />
-                            <WorkGridItem />
-                            <WorkGridItem />
-                            <WorkGridItem />
-                            <WorkGridItem />
-                            <WorkGridItem />
+                            <WorkGridItem title='Weather App' thumbnail={weatherApp} url='https://weather.qu1etboy.vercel.app/'>
+                                A simple Weather App built using NextJS and Tailwind CSS
+                            </WorkGridItem>
+                            <WorkGridItem title='Todo App' thumbnail={todo} url='https://react-todo-list-app-mu.vercel.app/'>
+                                A simple Todo App that can add and remove your task.
+                            </WorkGridItem>
                         </div>
                     </div>
                 </FadeIn>
