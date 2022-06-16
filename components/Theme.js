9a3788a90f169e3f2928118f935a4ac094554140
Qu1etboy <nonzagreanthai@gmail.com>
2022-06-16 22:58:00 +0700

    feat(theme): stored theme to localstorage

--- components/Theme.js
+++ components/Theme.js
@@ -4,7 +4,21 @@ export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
     const defaultTheme = 'dark';
-    const [theme, setTheme] = useState(defaultTheme);
+    const [theme, setTheme] = useState('');
+
+    useEffect(() => {
+        const savedTheme = window.localStorage.getItem("theme");
+        if (savedTheme !== null) {
+          setTheme(JSON.parse(savedTheme));
+        } else {
+          setTheme(defaultTheme);
+        }
+        // console.log(savedTheme);
+      }, []);
+    
+    useEffect(() => {
+    window.localStorage.setItem("theme", JSON.stringify(theme));
+    }, [theme]);
 
     const toggleTheme = () => {
         setTheme(theme === 'dark' ? 'light' : 'dark');
