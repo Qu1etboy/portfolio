c30471460a518e1ebf7e1d78835801d1060cf93d
Qu1etboy <nonzagreanthai@gmail.com>
2023-01-01 00:51:42 +0700

    feat: add meta tag

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,15 +1,18 @@
 import Head from "next/head";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { useRouter } from "next/router";
 
 const Container = (props) => {
   const { children, ...customMeta } = props;
+  const router = useRouter();
 
   const meta = {
     title: "Qu1etboy",
-    description: "Qu1etboy's Website",
+    description: "Qu1etboy's portfolio website",
     author: "Weerawong Vonggatunyu",
     type: "website",
+    image: "https://qu1etboy.vercel.app/static/images/qu1etboy.jpeg",
     ...customMeta,
   };
 
@@ -18,8 +21,28 @@ const Container = (props) => {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="follow, index" />
         <meta name="description" content={meta.description} />
+        <meta
+          name="keywords"
+          content="Qu1etboy, Portfolio, Developer, Programmer"
+        />
         <meta name="author" content={meta.author} />
+        <meta
+          property="og:url"
+          content={`https://qu1etboy.vercel.app/${router.asPath}`}
+        />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:site_name" content="Qu1etboy" />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@WeerawongNon" />
+        <meta name="twitter:image" content={meta.image} />
+
         <title>{meta.title}</title>
       </Head>
       <div className="flex flex-col min-h-screen relative text-gray-900 dark:text-gray-100">
