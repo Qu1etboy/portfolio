76515a79097ed75cc2cb59dec1940314810a2f2f
Qu1etboy <nonzagreanthai@gmail.com>
2023-05-30 01:14:18 +0700

    fix: add li to  ul

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,40 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const navItems = [
   {
-    href: "/",
-    name: "Home",
+    href: '/',
+    name: 'Home',
   },
   {
-    href: "/about",
-    name: "About",
+    href: '/about',
+    name: 'About',
   },
   {
-    href: "/projects",
-    name: "Projects",
+    href: '/projects',
+    name: 'Projects',
   },
 ];
 
 export default function Navbar() {
   const pathname = usePathname();
   return (
-    <header className="max-w-3xl mx-auto p-4">
+    <header className="mx-auto max-w-3xl p-4">
       <nav>
         <ul className="flex">
           {navItems.map((nav) => (
-            <Link
-              key={nav.href}
-              href={nav.href}
-              className={`${
-                pathname == nav.href ? "text-pink-500 font-medium" : ""
-              } hover:bg-pink-50 hover:text-pink-500 rounded-lg px-4 py-2 duration-200`}
-            >
-              {nav.name}
-            </Link>
+            <li key={nav.href}>
+              <Link
+                href={nav.href}
+                className={`${
+                  pathname == nav.href ? 'font-medium text-pink-500' : ''
+                } rounded-lg px-4 py-2 duration-200 hover:bg-pink-50 hover:text-pink-500`}
+              >
+                {nav.name}
+              </Link>
+            </li>
           ))}
         </ul>
       </nav>
