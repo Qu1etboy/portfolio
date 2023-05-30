ccb304b3b64f7f626e86ce6dfdd4766f32f9c4d9
Qu1etboy <nonzagreanthai@gmail.com>
2023-05-30 22:04:27 +0700

    feat: animated tabs on hover using framer-motion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,8 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 const navItems = [
   {
@@ -23,6 +25,7 @@ const navItems = [
 ];
 
 export default function Navbar() {
+  const [hoveredNav, setHoveredNav] = useState<string | null>(null);
   const pathname = usePathname();
   return (
     <header className="mx-auto max-w-3xl p-4">
@@ -32,11 +35,21 @@ export default function Navbar() {
             <li key={nav.href} className="py-2">
               <Link
                 href={nav.href}
+                onMouseEnter={() => setHoveredNav(nav.href)}
+                onMouseLeave={() => setHoveredNav(null)}
                 className={`${
                   pathname == nav.href ? 'font-medium text-pink-500' : ''
-                } rounded-lg px-4 py-2 duration-200 hover:bg-pink-50 hover:text-pink-500`}
+                } relative rounded-lg px-4 py-2 duration-200 hover:text-pink-500`}
               >
-                {nav.name}
+                {hoveredNav === nav.href && (
+                  <motion.span
+                    layoutId="bubble"
+                    className="absolute inset-0 bg-pink-100"
+                    style={{ borderRadius: 9999 }}
+                    transition={{ type: 'spring', bounce: 0.2, duration: 0.6 }}
+                  />
+                )}
+                <span className="relative z-10">{nav.name}</span>
               </Link>
             </li>
           ))}
