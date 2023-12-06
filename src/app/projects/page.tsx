5f75c7d50492886c10b1928428016712ed180f9e
Qu1etboy <nonzagreanthai@gmail.com>
2023-12-06 18:48:46 +0700

    chore: update project lists

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,11 +6,35 @@ import type { Metadata } from 'next';
 
 const projects = [
   {
+    id: 5,
+    name: 'KU Wongnai',
+    description:
+      'Website for finding foods and popular restaurants inside Kasetsart University.',
+    github: 'https://github.com/KU-Wongnai/ku-wongnai',
+    published: true,
+  },
+  {
     id: 1,
     name: 'Bangkok Air Quality Map',
     description: 'Show the air quality on the map in Bangkok',
     website: 'https://bangkokairqualitymap.vercel.app',
     github: 'https://github.com/Qu1etboy/bangkok-air-quality-map',
+    published: true,
+  },
+  {
+    id: 6,
+    name: 'Beam',
+    description: 'An event-organizing app.',
+    github: 'https://github.com/qu1etboy/beam',
+    published: true,
+  },
+  {
+    id: 7,
+    name: 'Tech Camp #1 Showcase',
+    description:
+      'Website that showcases participants’s projects from Tech Camp #1.',
+    website: 'https://camp.tech.nisit.ku.ac.th',
+    published: true,
   },
   {
     id: 2,
@@ -18,6 +42,7 @@ const projects = [
     description: 'A resume maker website',
     website: '',
     github: 'https://github.com/Qu1etboy/pm2.5-tracker',
+    published: false,
   },
   {
     id: 3,
@@ -25,13 +50,15 @@ const projects = [
     description: 'Know how much PM 2.5 is in your area',
     website: '',
     github: 'https://github.com/Qu1etboy/resume-edit',
+    published: true,
   },
   {
     id: 4,
     name: 'KU-Fondue',
     description:
-      'A Java desktop application for submit a complaint about Kasetsart University',
+      'A Java desktop application for submit a complaint about Kasetsart University.',
     github: 'https://github.com/Qu1etboy/KU-Fondue',
+    published: true,
   },
 ];
 
@@ -57,29 +84,36 @@ export default function Projects() {
         </Link>
       </p>
       <div className="mt-16 grid grid-cols-1 gap-y-3 sm:grid-cols-2 md:grid-cols-3">
-        {projects.map((project) => (
-          <div
-            key={project.id}
-            className="flex h-44 flex-col rounded-lg px-5 py-6 duration-300 hover:bg-gray-100"
-          >
-            <h2 className="mb-2 font-semibold">{project.name}</h2>
-            <p className="prose text-sm text-gray-800">{project.description}</p>
-            <div className="mt-auto flex items-center">
-              {project.website && (
-                <Link href={project.website} aria-label="website">
-                  <BiLink className="text-xl duration-300 hover:scale-125 hover:text-pink-500" />
-                </Link>
-              )}
-              <Link
-                href={project.github}
-                aria-label="github"
-                className="ml-auto"
+        {projects.map(
+          (project) =>
+            project.published && (
+              <div
+                key={project.id}
+                className="flex h-44 flex-col rounded-lg px-5 py-6 duration-300 hover:bg-gray-100/75"
               >
-                <FaGithub className="duration-300 hover:scale-125" />
-              </Link>
-            </div>
-          </div>
-        ))}
+                <h2 className="mb-2 font-semibold">{project.name}</h2>
+                <p className="prose text-sm tracking-tight text-gray-600">
+                  {project.description}
+                </p>
+                <div className="mt-auto flex items-center">
+                  {project.website && (
+                    <Link href={project.website} aria-label="website">
+                      <BiLink className="text-xl duration-300 hover:scale-125 hover:text-pink-500" />
+                    </Link>
+                  )}
+                  {project.github && (
+                    <Link
+                      href={project.github}
+                      aria-label="github"
+                      className="ml-auto"
+                    >
+                      <FaGithub className="duration-300 hover:scale-125" />
+                    </Link>
+                  )}
+                </div>
+              </div>
+            )
+        )}
       </div>
     </section>
   );
