4703458ccc36b794da4608e5e3a5fca100732a88
Qu1etboy <nonzagreanthai@gmail.com>
2024-02-24 01:38:37 +0700

    feat: move to astro and redesign the site

--- src/app/robots.ts
+++
@@ -1,9 +0,0 @@
-export default function robots() {
-  return {
-    rules: {
-      userAgent: '*',
-    },
-    sitemap: 'https://qu1etboy.dev/sitemap.xml',
-    host: 'https://qu1etboy.dev',
-  };
-}
--- src/app/sitemap.ts
+++
@@ -1,7 +0,0 @@
-export default function sitemap() {
-  const routes = ['', '/about', '/projects'];
-  return routes.map((route) => ({
-    url: `https://qu1etboy.dev${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }));
-}
